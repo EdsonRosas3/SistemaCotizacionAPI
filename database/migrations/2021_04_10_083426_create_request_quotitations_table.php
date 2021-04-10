29e3a7205a2d5ce7285c3423d17779defbdf11af
EdsonRosas3 <edsonrosas321@gmail.com>
2021-04-10 05:09:37 -0400

    se creo las migraciones, se implemento passport para la autenticacion

---
+++ database/migrations/2021_04_10_083426_create_request_quotitations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestQuotitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_quotitations', function (Blueprint $table) {
+            $table->increments('idRequest');
+            $table->string('nameUnidadGasto');
+            $table->string('aplicantName');
+            $table->date('requestDate');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_quotitations');
+    }
+}
